715377280cc8e6569d75a35849e131d7a2315df5
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-25 21:51:35 -0600

    adjusted links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './footer.css'
-import {AiOutlineFacebook, AiOutlineInstagram} from 'react-icons/ai'
-import {TfiTwitter} from 'react-icons/tfi'
 import {FiGithub, FiLinkedin} from 'react-icons/fi'
 
 function Footer() {
@@ -15,17 +13,9 @@ function Footer() {
        <li><a href="#portfolio">Portfolio</a></li>
        <li><a href="#contact">Contact</a></li>
       </ul>
-
-{/* TODO: change links to mine*/}
-
-      <div className="footer__socials">
-        <a href="https://facebook.com/isayaniphotography" target="_blank"><AiOutlineFacebook/></a>
-        <a href="https://instagram.com/insha.art" target="_blank"><AiOutlineInstagram/></a>
-        <a href="https://twitter.com/inshasayani" id='twitter__logo' target="_blank"><TfiTwitter/></a>
-        <span className="mobile__socials">
-        <a href="https://github.com/isayani" target="_blank"><FiGithub/></a>
-        <a href="https://linkedin.com/in/inshasayani" target="_blank"><FiLinkedin/></a>
-        </span>
+      <div className="mobile__socials footer__socials">
+        <a href="https://github.com/prappleman" target="_blank"><FiGithub/></a>
+        <a href="https://www.linkedin.com/in/parker-rappleye/" target="_blank"><FiLinkedin/></a>
       </div>
 
       <div className="footer__copyright">
