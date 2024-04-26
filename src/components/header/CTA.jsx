52cf5954ab27fab68e642506aea5299d2a570532
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-25 21:51:53 -0600

    setup resume download

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-// TODO: change resume
 import Resume from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Resume} download className='btn'>Résumé</a>
+        <a href={Resume} download className='btn'>Resume</a>
         <a href='#contact' className='btn btn-primary'>Connect</a>
     </div>
   )
