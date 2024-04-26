5c7cd9af5f70f12bce7735a0329588952ad4f3fe
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-25 21:52:35 -0600

    added some personal projects

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import "./project.css";
+import cover1 from "../../assets/LOOP.png";
+import cover2 from "../../assets/JATE.png";
+import cover3 from "../../assets/NOTE.png";
 
-// TODO: change project photos
-import cover1 from "../../assets/portfolio1.jpg";
-import cover2 from "../../assets/portfolio2.jpg";
-import cover3 from "../../assets/portfolio3.jpg";
-import cover4 from "../../assets/DFQ2.gif";
-import cover5 from "../../assets/portfolio5.png";
-import cover6 from "../../assets/portfolio6.jpg";
-
-// IMAGES FOR PRIVATE USE ONLY (NO CREATIVE COMMONS)
-
-// TODO: change project links to mine
+// IMAGES FOR PRIVATE USE ONLY
 
 const data = [
   {
@@ -24,37 +17,16 @@ const data = [
   {
     id: 2,
     image: cover2,
-    title: "Mix & Match",
-    github: "https://github.com/isayani/cocktail-music-generator",
-    demo: "https://isayani.github.io/cocktail-music-generator/",
+    title: "J.A.T.E.",
+    github: "https://github.com/prappleman/PWA-Text-Editor",
+    demo: "https://text-editor-pwa12-ed67e7c236f3.herokuapp.com",
   },
   {
     id: 3,
     image: cover3,
-    title: "URB+SAGE Apothecary",
-    github: "https://github.com/isayani",
-    demo: "https://urbnsage.com",
-  },
-  {
-    id: 4,
-    image: cover4,
-    title: "Dhul-Faqar Label",
-    github: "https://github.com/isayani",
-    demo: "https://dhulfaqar.com",
-  },
-  {
-    id: 5,
-    image: cover5,
-    title: "Veriti",
-    github: "https://github.com/veriti-app/veriti",
-    demo: "https://veriti-app.herokuapp.com/",
-  },
-  {
-    id: 6,
-    image: cover6,
-    title: "Daily Planner",
-    github: "https://github.com/isayani/third-party-api-day-planner",
-    demo: "https://isayani.github.io/third-party-api-day-planner/",
+    title: "Note-Taker",
+    github: "https://github.com/prappleman/Note-Taker",
+    demo: "https://note-taker-brry.onrender.com/notes",
   },
 ];
 
