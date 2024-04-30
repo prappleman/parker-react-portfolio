1e191edd11a326d4eff4845238098e49c655d31a
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-29 19:44:16 -0600

    fixed imports

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import {useState} from 'react';
 import './nav.css';
 import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail, BiCodeCurly} from 'react-icons/bi';
 import {RiServiceLine} from 'react-icons/ri';
 import {Link} from 'react-scroll';
 
 function Nav() {
-  const [activeNav, setActiveNav] = useState('#')
-  // return (
-  //   <nav>
-  //     <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
-  //     <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
-  //     <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeCurly/></a>
-  //     <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
-  //     <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiBook/></a>
-  //     <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
-  //   </nav>
-  // )
   return (
     <nav>
       <Link to="header" spy={true} smooth={true} duration={500} activeClass='active' ><BiHomeAlt/></Link>
